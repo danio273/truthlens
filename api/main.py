5ddf558fd8333ecccf44a45e58430a31318b9378
danio273 <215408284+danio273@users.noreply.github.com>
2025-11-21 17:39:34 +0100

    feat(api): add limited CORS for extension

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from models import FactCheckRequest
 from factchecker import fact_check
 
@@ -7,6 +8,13 @@ app = FastAPI(
     docs_url=None,
     redoc_url=None,
     openapi_url=None
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["POST", "GET", "HEAD"],
+    allow_headers=["Content-Type"],
 )
 
 @app.post("/")
