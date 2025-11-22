19df80e850101938d2f7d9dd07b6a86c6e2d29ed
danio273 <215408284+danio273@users.noreply.github.com>
2025-11-22 12:10:36 +0100

    Initial commit for app

---
+++ app/lib/app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme lightColorScheme = ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.light,
+    );
+    ColorScheme darkColorScheme = ColorScheme.fromSeed(
+      seedColor: Colors.deepPurple,
+      brightness: Brightness.dark,
+    );
+
+    return MaterialApp(
+      title: 'TruthLens',
+      theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      themeMode: ThemeMode.system,
+      home: const Placeholder(),
+    );
+  }
+}
