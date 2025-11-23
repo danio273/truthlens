962242a6b0aae719b0c08120d1fbe29a13196923
danio273 <215408284+danio273@users.noreply.github.com>
2025-11-23 14:09:16 +0100

    fix(app): remove setPathUrlStrategy to fix routing issues

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 
 void main() {
-  setPathUrlStrategy();
-  
   runApp(const MyApp());
 }
